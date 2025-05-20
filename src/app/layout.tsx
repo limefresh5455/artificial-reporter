c04d8ef8513fd020b5a667b3c4de5b47ce64bd88
limefresh5455 <limefresh5455@gmail.com>
2025-05-20 18:07:40 +0530

    newsletter dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5403082360131543"
+                    crossorigin="anonymous"></script>
+                    
                 {/* <Script
                     strategy="afterInteractive"
                     src="https://www.googletagmanager.com/gtag/js?id=G-9K8H9311HB"
